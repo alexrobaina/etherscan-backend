9c04cf5e9337219720cca5a07733330e870b1219
alexrobaina <alexrobainaph@gmail.com>
2024-01-16 11:00:21 -0300

    fix docker

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  APP_NAME: 'Securitize',
+  APP_NAME: 'etherscan',
   HOST: process.env.HOST || 'http://localhost',
   PORT: process.env.PORT || '3011',
   EXPIRES_IN: 86400,
--- src/routes/ether.ts
+++ src/routes/ether.ts
@@ -2,7 +2,7 @@ import {
   create,
   getAllExchangeRates,
   getEtherExchangeRate,
-} from './../useCases/etherCases/etherController'
+} from '../useCases/etherCases/etherController'
 import express from 'express'
 
 import { verifyToken } from '../middlewares/auth'
